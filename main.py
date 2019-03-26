3b71475a938d1af3843765a43db9cc436a4f7714
Jan Wilczek <janekw@vp.pl>
2019-03-26 14:53:29 +0100

    Initial game loop.

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
-import pygame,  sys, os
-from pygame.locals import *
+import pygame
 
-pygame.init()
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((400, 300))
+    done = False
+
+    while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+
+        pygame.draw.rect(screen, (0, 128, 255), pygame.Rect(30, 30, 60, 60))
+        pygame.display.flip()
+
+if __name__ == '__main__':
+    main()
+

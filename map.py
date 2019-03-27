999bea54fe5f683fa1edd90f852071235172ead0
Shomrey <szachym1@gmail.com>
2019-03-27 15:52:53 +0100

    add beter terrain colison and movement

--- map.py
+++ map.py
@@ -2,7 +2,7 @@ import sys, pygame
 pygame.init()
 
 size = width, height = 760, 520
-speed = [2, 2]
+speed = [0, 0]
 bg = pygame.transform.scale(pygame.image.load("bg.jpg"), [760, 520])
 black = 0, 0, 0
 
@@ -22,18 +22,27 @@ screen = pygame.display.set_mode(size)
 
 hero = pygame.transform.scale(pygame.image.load("player.jpg"), [50, 70])
 herorect = hero.get_rect()
-
+herorect.y = 245
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
-
+        if event.type == 26:
+            speed[1] = 1
+            pygame.time.set_timer(26, 0)
+    key=pygame.key.get_pressed()
+    if key[pygame.K_RIGHT]: speed[0] = 1
+    if key[pygame.K_LEFT]: speed[0] = -1
+    if key[pygame.K_UP]:
+        speed[1] = -1
+        pygame.time.set_timer(26, 1000)
+    if key[pygame.K_DOWN]: speed[1] = 1
     herorect = herorect.move(speed)
     if herorect.left < 0 or herorect.right > width:
-        speed[0] = -speed[0]
+        speed[0] = 0
     if herorect.top < 0 or herorect.bottom > height:
-        speed[1] = -speed[1]
+        speed[1] = 0
     if collide(herorect):
-        speed[1] = -speed[1]
+        speed[1] = 0
     screen.fill(black)
     screen.blit(bg,(0, 0))
     screen.blit(hero, herorect)

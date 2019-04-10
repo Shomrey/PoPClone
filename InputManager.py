6953b5b536f6c2e08d05ebc670d4bcbab8d4bd9a
Krzys01k <46790401+Krzys01k@users.noreply.github.com>
2019-04-11 01:01:05 +0200

    InputManager added queue-based jump
    _jump_queue determines next relative positions, _jump_x decides about jumping left/right. Function _jump_continue() sets relative position

--- InputManager.py
+++ InputManager.py
@@ -1,19 +1,21 @@
 import pygame
+import queue
 
 
 class InputManager:
     def __init__(self, player):
         self._player = player
-        self._jumping = False
+        self._jump_queue = queue.Queue()
+        self._jump_x = 0
         self._crouching = False
         self._none_pressed = (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
 
     def on_update(self):
         pressed = pygame.key.get_pressed()
 
-        if self._jumping:
+        if self._jump_queue.empty():
             pressed = self._none_pressed
-            self._jumping = False
+            self._jump_continue()
 
         if pressed[pygame.K_LEFT] and pressed[pygame.K_RIGHT]: pass
         elif pressed[pygame.K_LEFT]: self._move_left()
@@ -30,16 +32,25 @@ class InputManager:
                 self._stand_up()
 
     def _jump_up(self):
-        self._player.set_position_relative(None, -3)
-        self._jumping = True
+        jump_values = (-5, -2, -1, 0, 1, 2, 5)
+        for i in jump_values: self._jump_queue.put(i)
+        self._jump_x = 0
+        self._jump_continue()
 
     def _jump_left(self):
-        self._player.set_position_relative(None, -3)
-        self._jumping = True
+        jump_values = (-3, -1, 0, 0, 0, 1, 3)
+        for i in jump_values: self._jump_queue.put(i)
+        self._jump_x = -3
+        self._jump_continue()
 
     def _jump_right(self):
-        self._player.set_position_relative(None, -3)
-        self._jumping = True
+        jump_values = (-3, -1, 0, 0, 0, 1, 3)
+        for i in jump_values: self._jump_queue.put(i)
+        self._jump_x = 3
+        self._jump_continue()
+
+    def _jump_continue(self):
+        self._player.set_position_relative(self._jump_x, self._jump_queue.get())
 
     def _move_left(self):
         self._player.set_position_relative(-3, None)

6c5c7d15621abac7805ebb22530cf25dd30f4e05
Krzys01k <46790401+Krzys01k@users.noreply.github.com>
2019-04-11 08:09:57 +0200

    Update InputManager.py

--- InputManager.py
+++ InputManager.py
@@ -25,9 +25,11 @@ class InputManager:
         elif pressed[pygame.K_RIGHT] and pressed[pygame.K_UP]: self._jump_right()
         elif pressed[pygame.K_UP]: self._jump_up()
 
-        if self._crouching:
-            if pressed[pygame.K_DOWN]: self._crouch()
-            else: self._stand_up()
+        if pressed[pygame.K_DOWN]:
+            self._crouch()
+        else:
+            if self._crouching:
+                self._stand_up()
 
     def _jump_up(self):
         jump_values = (-9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 0)
@@ -60,7 +62,6 @@ class InputManager:
 
     def _crouch(self):
         self._crouching = True
-        self._player.set_position_relative(None, 3)
 
     def _stand_up(self):
         self._crouching = False

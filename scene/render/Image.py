98667701ece7513676e571bfa7e609565a8512b3
Jan Wilczek <janekw@vp.pl>
2019-06-05 16:05:42 +0200

    Nearly great cropping - handle flipping the image.

--- scene/render/Image.py
+++ scene/render/Image.py
@@ -12,7 +12,18 @@ class Image(Renderable):
             self._image = pygame.transform.flip(self._image, False, True)
         self._rect = pygame.rect.Rect(x, y, width, height)
         if clip_rect:
+            original_rect = self._rect.copy()
             self._rect = self._rect.clip(clip_rect)
+
+            # crop the image
+            crop_area_width = self._rect.width / original_rect.width * self._image.get_width()
+            crop_area_height = self._rect.height / original_rect.height * self._image.get_height()
+
+            crop_area = (self._rect.x - original_rect.x, self._rect.y - original_rect.y, crop_area_width, crop_area_height)
+            cropped_image = pygame.Surface((crop_area_width, crop_area_height))
+            cropped_image.blit(self._image, (0, 0), crop_area)
+            self._image = cropped_image
+
 
     def on_render(self, screen, screenshot_resolution, screenshot_x_offset):
         rect = Renderable.to_screen_rect(self._rect, screen.get_rect(), screenshot_resolution, screenshot_x_offset)

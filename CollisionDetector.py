cab9a21e2ba18c859e3e6ef9b6e522c9711c42d1
Krzys <jtkrzys@gmail.com>
2019-06-06 11:55:03 +0200

    tweaked climbing

--- CollisionDetector.py
+++ CollisionDetector.py
@@ -67,9 +67,8 @@ class CollisionDetector:
             raise NotImplementedError
 
     def check_can_climb(self):
-        rect1 = pygame.Rect(self._player.get_position()[0]+20, self._player.get_position()[1]+100, 10, 2)
+        rect1 = pygame.Rect(self._player.get_position()[0], self._player.get_position()[1]-30, 50, 10)
         return rect1.collidelist(self._scene_floors) < 0
-        # ???
 
     def update_floors(self, scene_floors):
         self._scene_floors = scene_floors
